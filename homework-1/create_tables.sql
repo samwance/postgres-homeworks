c868d2643a051fa0f7db4fee9457de9832004098
semyon <odintsovvans@gmail.com>
2023-08-09 14:59:49 +0600

    hw_1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,29 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE customers
+(
+	customer_id varchar(100) NOT NULL,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE employees
+(
+	employee_id SERIAL PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date DATE,
+	notes TEXT
+);
+
+CREATE TABLE orders
+(
+	order_id SERIAL PRIMARY KEY,
+	customer_id varchar(100),
+	employee_id int,
+	order_date DATE,
+	ship_city varchar(100),
+	FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+	FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,47 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+CUSTOMERS_PATH = 'north_data/customers_data.csv'
+EMPLOYEES_PATH = 'north_data/employees_data.csv'
+ORDERS_PATH = 'north_data/orders_data.csv'
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="12345"
+)
+
+cur = conn.cursor()
+
+with open(CUSTOMERS_PATH, newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    next(reader)  # Пропуск заголовка
+    for row in reader:
+        # Формирование SQL-запроса на добавление данных
+        sql = "INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s)"
+        # Выполнение SQL-запроса с передачей параметров
+        cur.execute(sql, (row[0], row[1], row[2]))
+        
+with open(EMPLOYEES_PATH, newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    next(reader)  # Пропуск заголовка
+    for row in reader:
+        # Формирование SQL-запроса на добавление данных
+        sql = "INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)"
+        # Выполнение SQL-запроса с передачей параметров
+        cur.execute(sql, (row[0], row[1], row[2], row[3], row[4], row[5]))
+
+with open(ORDERS_PATH, newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    next(reader)  # Пропуск заголовка
+    for row in reader:
+        # Формирование SQL-запроса на добавление данных
+        sql = "INSERT INTO orders VALUES (%s, %s, %s, %s, %s)"
+        # Выполнение SQL-запроса с передачей параметров
+        cur.execute(sql, (row[0], row[1], row[2], row[3], row[4]))
+
+conn.commit()
+cur.close()
+conn.close()
